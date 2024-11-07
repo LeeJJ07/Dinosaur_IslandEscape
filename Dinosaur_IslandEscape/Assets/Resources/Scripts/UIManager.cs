bd057fd01bbf9cec9633df70e93746640cb6695a
HakSeung <fallro624@gmail.com>
2024-11-07 17:22:26 +0900

    feat: UIManager 생성

---
+++ Dinosaur_IslandEscape/Assets/Resources/Scripts/UIManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HakSeung
+{
+    public class UIManager : MonoBehaviour
+    {
+        private const string uiManagerObjectName = "_UIManager";
+        private static UIManager s_Instance;
+
+        public static UIManager Instance
+        {
+            get
+            {
+                if(s_Instance == null)
+                {
+                    GameObject newUIManagerObject = new GameObject(uiManagerObjectName);
+                    s_Instance = newUIManagerObject.AddComponent<UIManager>();
+                }
+                return s_Instance;
+            }
+        }
+        private void Awake()
+        {
+            if (s_Instance != null && s_Instance != this)
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+            s_Instance = this;
+
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+    }
+}
