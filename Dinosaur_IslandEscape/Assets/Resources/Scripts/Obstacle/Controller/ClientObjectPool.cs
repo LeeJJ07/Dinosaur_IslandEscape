267ecad27172b016a3ea58d2e732046aa8c9fc5d
MyeongjinMoon <114853338+MyeongjinMoon@users.noreply.github.com>
2024-11-11 00:46:11 +0900

    feat: 장애물 생성 확률 추가

--- Dinosaur_IslandEscape/Assets/Resources/Scripts/Obstacle/Controller/ClientObjectPool.cs
+++ Dinosaur_IslandEscape/Assets/Resources/Scripts/Obstacle/Controller/ClientObjectPool.cs
@@ -4,25 +4,74 @@ using UnityEngine;
 
 namespace MyeongJin
 {
-    public class ClientObjectPool : MonoBehaviour
-    {
-        private CObstacleObjectPool obstaclePool;
-        private CCreatureHerdPool creatureHerdPool;
+	public class ClientObjectPool : MonoBehaviour
+	{
+		private CObstacleObjectPool obstaclePool;
+		private CCreatureHerdPool creatureHerdPool;
+
+		private int playerCount = 2;
+		private float[] obstacleLine;
+		private float[] creatureHerdLine;
+        private System.Random random = new System.Random();
 
         private void Start()
-        {
+		{
+            obstacleLine = new float[playerCount];
+            for (int i = 0; i < playerCount; i++)
+                obstacleLine[i] = 0.2f;
+
+            creatureHerdLine = new float[playerCount];
+            for (int i = 0; i < playerCount; i++)
+                creatureHerdLine[i] = 0.2f;
+
             obstaclePool = gameObject.AddComponent<CObstacleObjectPool>();
-            creatureHerdPool = gameObject.AddComponent<CCreatureHerdPool>();
-        }
+			creatureHerdPool = gameObject.AddComponent<CCreatureHerdPool>();
+		}
 
-        // TODO < 문명진 > - OnGUI 메소드 내용을 다른 호출 메소드(Update or Observer)로 변경해야함. - 2024.11.09 17:05
-        private void OnGUI()
+		// TODO < 문명진 > - OnGUI 메소드 내용을 다른 호출 메소드(Update or Observer)로 변경해야함. - 2024.11.09 17:05
+		private void OnGUI()
+		{
+			if (GUILayout.Button("Spawn Obstacle"))
+			{
+				for (int i = 0; i < playerCount; i++)
+				{
+					if (CanSpawnObstacle(obstacleLine[i]))
+					{
+						obstaclePool.SpawnObstacle();
+						ResetChance(obstacleLine, i);
+					}
+					else
+						ChanceUp(obstacleLine, i);
+				}
+			}
+
+			if (GUI.Button(new Rect(10, 10, 50, 50), "Spawn Pteranodon"))
+			{
+                for (int i = 0; i < playerCount; i++)
+                {
+                    if (CanSpawnObstacle(creatureHerdLine[i]))
+                    {
+                        creatureHerdPool.SpawnPteranodon();
+                        ResetChance(creatureHerdLine, i);
+                    }
+                    else
+                        ChanceUp(creatureHerdLine, i);
+                }
+            }
+		}
+
+		bool CanSpawnObstacle(float chance)
+		{
+            float randomValue = (float)random.NextDouble();
+			return randomValue < chance;
+        }
+        void ResetChance(float[] arr, int index)
         {
-            if (GUILayout.Button("Spawn Obstacle"))
-                obstaclePool.SpawnObstacle();
-
-            if (GUI.Button(new Rect(10, 10, 50, 50), "Spawn Pteranodon"))
-                creatureHerdPool.SpawnPteranodon();
+            arr[index] = 0.2f;
         }
+        void ChanceUp(float[] arr, int index)
+		{
+            arr[index] += 0.2f;
+		}
     }
 }
--- Dinosaur_IslandEscape/Assets/Resources/Scripts/Obstacle/Team/CCreatureHerd.cs
+++ Dinosaur_IslandEscape/Assets/Resources/Scripts/Obstacle/Team/CCreatureHerd.cs
@@ -10,27 +10,17 @@ namespace MyeongJin
         public IObjectPool<CCreatureHerd> Pool { get; set; }
 
         private float timeToCheckPosition = 1f;
-        private int maxRotateValue = 1;
-        private int minRotateSpeed = 4;
-        private int curRotateSpeed;
+        private float moveSpeed = 10f;
 
         //TODO < 문명진 > - startPosition Value를 현재 자기의 위치로 변경해야함. - 2024.11.07 4:10
         [SerializeField]
-        private Vector3 startPosition = new Vector3(0, 10, 30);
+        private Vector3 startPosition = new Vector3(0, 10, 20);
         // <<
 
         //TODO < 문명진 > - destructPosition Value를 Dinosaur의 끝 부분으로 변경해야함. - 2024.11.07 4:10
         [SerializeField]
-        private int destructPosition = -10;
+        private int destructPosition = -5;
         // <<
-
-        private Vector3 curPosition;
-
-        private void Start()
-        {
-            curPosition = startPosition;
-            curRotateSpeed = Random.Range(minRotateSpeed, minRotateSpeed + maxRotateValue);
-        }
 
         private void Update()
         {
@@ -46,7 +36,7 @@ namespace MyeongJin
 
             StartCoroutine(CheckPosition());
 
-            if (curPosition.z < destructPosition)
+            if (this.transform.position.z < destructPosition)
                 ReturnToPool();
         }
 
@@ -61,15 +51,13 @@ namespace MyeongJin
         }
         private void ResetObstacle()
         {
-            //TODO < 문명진 > - 돌 rotation 초기화 넣어야함. - 2024.11.07 4:20
 
-            //TODO < 문명진 > - 돌의 삭제 위치를 공룡 위치로 초기화 해줘야함. - 2024.11.07 4:20
         }
         private void FlyPteranodon()
         {
             Debug.Log("Fly Pteranodon!");
 
-            this.transform.Translate(Vector3.forward * Time.deltaTime * -1);
+            this.transform.Translate(Vector3.forward * Time.deltaTime * -moveSpeed);
         }
     }
 }
