c40d811eb216f9c2eb8a0439722d757d43e34e18
Jaehoon <jjh951110@naver.com>
2024-11-11 19:45:57 +0900

    feat : 브랜치 새로 파고, 에셋 추가

---
+++ Dinosaur_IslandEscape/Assets/Resources/Scripts/Map/TileManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JaeHoon
+{
+    public class TileManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] tilePrefabs;
+        [SerializeField] private float tileRespawnPoint = 0.0f;
+        [SerializeField] private float tileLength = 16.0f;
+        [SerializeField] private int numberOfTiles = 3;
+        [SerializeField] private Transform firstPlayerTransform;
+        [SerializeField] private Transform secondPlayerTransform;
+        private List<GameObject> activeTiles = new List<GameObject>();
+        private float preventSinkhole = 25.0f;
+
+        private void Start()
+        {
+            for (int index = 0; index < numberOfTiles; index++)
+            {
+                SpawnTile(Random.Range(0, tilePrefabs.Length));
+            }
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (firstPlayerTransform.position.z - preventSinkhole > tileRespawnPoint - (numberOfTiles * tileLength)
+                || secondPlayerTransform.position.z - preventSinkhole > tileRespawnPoint - (numberOfTiles * tileLength))
+            {
+                SpawnTile(Random.Range(0, tilePrefabs.Length));
+                DeleteTile();
+            }
+        }
+
+        private void SpawnTile(int tileIndex)
+        {
+            GameObject playerRun = Instantiate(tilePrefabs[tileIndex], transform.forward * tileRespawnPoint, transform.rotation);
+            activeTiles.Add(playerRun);
+            tileRespawnPoint += tileLength;
+        }
+        private void DeleteTile()
+        {
+            Destroy(activeTiles[0]);
+            activeTiles.RemoveAt(0);
+        }
+    }
+}
