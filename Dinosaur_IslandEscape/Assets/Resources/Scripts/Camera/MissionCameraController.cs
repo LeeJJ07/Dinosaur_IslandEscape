0fae0b5f585a6f55a2bd5923c6608df9c8ab39ef
LeeJJ07 <jkj9513@naver.com>
2024-11-20 16:57:39 +0900

    fix: timeline 컷씬 연출 마무리

---
+++ Dinosaur_IslandEscape/Assets/Resources/Scripts/Camera/MissionCameraController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JongJin
+{
+    public class MissionCameraController : MonoBehaviour
+    {
+        [SerializeField] private GameObject target;
+        [SerializeField] private GameObject follow;
+
+        private Vector3 targetPos;
+
+    }
+}
