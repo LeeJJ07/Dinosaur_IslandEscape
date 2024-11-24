50d2b603c9f439c692ee62cac7999d172f60ece1
blueskys95 <jjh951110@naver.com>
2024-11-25 01:33:03 +0900

    feat : TileManager의 TilePrefab을 없어지는 방식이 아니라 앞으로 땡기는 오브젝트 형식으로 변경

--- Dinosaur_IslandEscape/Assets/Resources/Scripts/Map/TileManager.cs
+++ Dinosaur_IslandEscape/Assets/Resources/Scripts/Map/TileManager.cs
@@ -14,7 +14,7 @@ namespace JaeHoon
         [SerializeField] private Transform firstPlayerTransform;
         [SerializeField] private Transform secondPlayerTransform;
         private List<GameObject> activeTiles = new List<GameObject>();
-        private float preventSinkhole = 25.0f;
+        private float preventSinkhole = 135.0f;
 
         private void Start()
         {
@@ -24,14 +24,13 @@ namespace JaeHoon
             }
         }
 
-        // Update is called once per frame
         private void Update()
         {
             if (firstPlayerTransform.position.z - preventSinkhole > tileRespawnPoint - (numberOfTiles * tileLength)
-                || secondPlayerTransform.position.z - preventSinkhole > tileRespawnPoint - (numberOfTiles * tileLength))
+                || secondPlayerTransform.position.z - preventSinkhole > tileRespawnPoint - (numberOfTiles * tileLength))                    // 플레이어 위치를 기준으로 타일을 추가 생성하고 오래된 타일을 재배치
             {
                 SpawnTile(Random.Range(0, tilePrefabs.Length));
-                DeleteTile();
+                RepositionTile();                                   // 타일 재배치
             }
         }
 
@@ -41,10 +40,59 @@ namespace JaeHoon
             activeTiles.Add(playerRun);
             tileRespawnPoint += tileLength;
         }
-        private void DeleteTile()
+
+        private void RepositionTile()
         {
-            Destroy(activeTiles[0], 300f);            // Destroy 대신 사라질 타일을 player 앞으로 가져오는 형식으로
+            GameObject oldestTile = activeTiles[0];         // 가장 오래된 타일을 재사용하여 앞으로 이동
             activeTiles.RemoveAt(0);
+
+            oldestTile.transform.position = transform.forward * tileRespawnPoint;               // 타일을 새 위치로 재배치
+            tileRespawnPoint += tileLength;
+
+            activeTiles.Add(oldestTile);                 // 재배치된 타일을 리스트의 끝에 추가
         }
     }
 }
+
+//public class TileManager : MonoBehaviour
+//{
+//    [SerializeField] private GameObject[] tilePrefabs;
+//    [SerializeField] private float tileRespawnPoint = 0.0f;
+//    [SerializeField] private float tileLength = 16.0f;
+//    [SerializeField] private int numberOfTiles = 3;
+//    [SerializeField] private Transform firstPlayerTransform;
+//    [SerializeField] private Transform secondPlayerTransform;
+//    private List<GameObject> activeTiles = new List<GameObject>();
+//    private float preventSinkhole = 25.0f;
+
+//    private void Start()
+//    {
+//        for (int index = 0; index < numberOfTiles; index++)
+//        {
+//            SpawnTile(Random.Range(0, tilePrefabs.Length));
+//        }
+//    }
+
+//    // Update is called once per frame
+//    private void Update()
+//    {
+//        if (firstPlayerTransform.position.z - preventSinkhole > tileRespawnPoint - (numberOfTiles * tileLength)
+//            || secondPlayerTransform.position.z - preventSinkhole > tileRespawnPoint - (numberOfTiles * tileLength))
+//        {
+//            SpawnTile(Random.Range(0, tilePrefabs.Length));
+//            DeleteTile();
+//        }
+//    }
+
+//    private void SpawnTile(int tileIndex)
+//    {
+//        GameObject playerRun = Instantiate(tilePrefabs[tileIndex], transform.forward * tileRespawnPoint, transform.rotation);
+//        activeTiles.Add(playerRun);
+//        tileRespawnPoint += tileLength;
+//    }
+//    private void DeleteTile()
+//    {
+//        Destroy(activeTiles[0], 300f);            // Destroy 대신 사라질 타일을 player 앞으로 가져오는 형식으로
+//        activeTiles.RemoveAt(0);
+//    }
+//}
