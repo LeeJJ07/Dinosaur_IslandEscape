acc85295e1e2c1d723289fdfa98d8258714b4204
Jaehoon <jjh951110@naver.com>
2024-11-25 13:49:27 +0900

    fix : TileManager에서 TilePrefabs이 생성되고 없어지는 방식에서, 오브젝트 풀링 형식으로 뒤에 있는 TilePrefab을 앞으로 당겨오게 함.

--- Dinosaur_IslandEscape/Assets/Resources/Scripts/Map/TileManager.cs
+++ Dinosaur_IslandEscape/Assets/Resources/Scripts/Map/TileManager.cs
@@ -14,7 +14,7 @@ namespace JaeHoon
         [SerializeField] private Transform firstPlayerTransform;
         [SerializeField] private Transform secondPlayerTransform;
         private List<GameObject> activeTiles = new List<GameObject>();
-        private float preventSinkhole = 25.0f;
+        private float preventSinkhole = 135.0f;                 // 타일이 옮겨지기 전 player가 떨어짐을 막기 위함
 
         private void Start()
         {
@@ -24,14 +24,13 @@ namespace JaeHoon
             }
         }
 
-        // Update is called once per frame
         private void Update()
         {
             if (firstPlayerTransform.position.z - preventSinkhole > tileRespawnPoint - (numberOfTiles * tileLength)
-                || secondPlayerTransform.position.z - preventSinkhole > tileRespawnPoint - (numberOfTiles * tileLength))
+                || secondPlayerTransform.position.z - preventSinkhole > tileRespawnPoint - (numberOfTiles * tileLength))                    // 플레이어 위치를 기준으로 타일을 추가 생성하고 오래된 타일을 재배치
             {
                 SpawnTile(Random.Range(0, tilePrefabs.Length));
-                DeleteTile();
+                RepositionTile();                                   // 타일 재배치
             }
         }
 
@@ -41,10 +40,16 @@ namespace JaeHoon
             activeTiles.Add(playerRun);
             tileRespawnPoint += tileLength;
         }
-        private void DeleteTile()
+
+        private void RepositionTile()
         {
-            Destroy(activeTiles[0], 300f);            // Destroy 대신 사라질 타일을 player 앞으로 가져오는 형식으로
+            GameObject oldestTile = activeTiles[0];         // 가장 오래된 타일을 재사용하여 앞으로 이동
             activeTiles.RemoveAt(0);
+
+            oldestTile.transform.position = transform.forward * tileRespawnPoint;               // 타일을 새 위치로 재배치
+            tileRespawnPoint += tileLength;
+
+            activeTiles.Add(oldestTile);                 // 재배치된 타일을 리스트의 끝에 추가
         }
     }
 }
